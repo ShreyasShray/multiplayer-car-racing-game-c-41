f235a2cfa1b1930315be3192b44f03d6f98f6f63
whitehatjr <74046171+ShreyasShray@users.noreply.github.com>
2020-12-26 13:43:44 +0530

    Add files via upload

---
+++ js/Form.js
@@ -0,0 +1,56 @@
+class Form {
+
+  constructor() {
+    this.input = createInput("Name");
+    this.button = createButton('Play');
+    this.greeting = createElement('h2');
+    this.title = createElement('h2');
+    this.reset = createButton('Reset');
+  }
+  hide(){
+    this.greeting.hide();
+    this.button.hide();
+    this.input.hide();
+    this.title.hide();
+  }
+
+  display(){
+    this.title.html("Car Racing Game");
+    this.title.position(displayWidth/2 - 50, 0);
+
+    this.input.position(displayWidth/2 - 40 , displayHeight/2 - 80);
+    this.button.position(displayWidth/2 + 30, displayHeight/2);
+    this.reset.position(displayWidth-100,20);
+
+    this.button.mousePressed(()=>{
+      this.input.hide();
+      this.button.hide();
+      player.name = this.input.value();
+      playerCount+=1;
+      player.index = playerCount;
+      player.update();
+      player.updateCount(playerCount);
+      this.greeting.html("Hello " + player.name)
+      this.greeting.position(displayWidth/2 - 70, displayHeight/4);
+    });
+
+    this.reset.mousePressed(()=>{
+      Player.updateCarsAtEnd(0);
+      player.updateCount(0);
+      game.update(0);
+    });
+
+  }
+
+  displayRank(){
+    background("white");
+    this.title.html("Car racing game");
+    this.title.position(displayWidth/2 -50, 100);
+    var message = createElement("h2");
+    message.html("Congratulations " + player.name);
+    message.position(250, displayHeight/2 - 80);
+    var message2 = createElement("h2");
+    message2.html("You attained position " + player.rank);
+    message2.position(250, displayHeight/2 - 20);
+  }
+}
